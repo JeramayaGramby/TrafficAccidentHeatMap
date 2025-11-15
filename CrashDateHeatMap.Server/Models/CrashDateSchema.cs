4346e1bdec3b0f7af40dd41bea980a7ad23b1553
Jeramaya <jgramby@wgu.edu>
2025-11-15 11:20:22 -0500

    Changed Data Types of objects inside CrashDateSchema to list representations.

--- CrashDateHeatMap.Server/Models/CrashDateSchema.cs
+++ CrashDateHeatMap.Server/Models/CrashDateSchema.cs
@@ -5,43 +5,70 @@ namespace CrashDateHeatMap.Server.Models
 {
     public struct CrashDateSchema
     {
-        public int CrashRecordNumber;
-        public string? District;
-        public string? CrashCounty;
-        public string? Municipality;
-        public DateTime CrashDate;
-        public byte CrashSceneLighting;
-        public byte Weather;
-        public byte RoadCondition;
-        public byte CollisionType;
-        public ushort RelationToRoad;
-        public byte IntersectionType;
-        public byte TrafficControlDeviceType;
-        public byte UrbanRural;
-        public byte LocationType;
-        public bool SchoolBusInvolved;
-        public bool SchoolZone;
-        public byte PersonCount;
-        public byte VehicleCount;
-        public byte FatalCount;
-        public byte InjuryCount;
-        public byte PedestrianCount;
-        public byte PedestrianDeathCount;
-        public double PoliceReportedLatitude;
-        public double PoliceReportedLongitude;
-        public double SecondaryResponderLatitude;
-        public double SecondaryResponderLongitude;
-        public bool PhantomVehicleInvolved;
-        public byte SpeedLimit;
-        public string? StreetName;
-        public bool ImpairedDriver;
+        public List<int> CrashRecordNumber;
+        public List<string?> District;
+        public List<string?> CrashCounty;
+        public List<string?> Municipality;
+        public List<DateTime> CrashDate;
+        public List<byte> CrashSceneLighting;
+        public List<byte> Weather;
+        public List<byte> RoadCondition;
+        public List<byte> CollisionType;
+        public List<ushort> RelationToRoad;
+        public List<byte> IntersectionType;
+        public List<byte> TrafficControlDeviceType;
+        public List<byte> UrbanRural;
+        public List<byte> LocationType;
+        public List<bool> SchoolBusInvolved;
+        public List<bool> SchoolZone;
+        public List<byte> PersonCount;
+        public List<byte> VehicleCount;
+        public List<byte> FatalCount;
+        public List<byte> InjuryCount;
+        public List<byte> PedestrianCount;
+        public List<byte> PedestrianDeathCount;
+        public List<double> PoliceReportedLatitude;
+        public List<double> PoliceReportedLongitude;
+        public List<double> SecondaryResponderLatitude;
+        public List<double> SecondaryResponderLongitude;
+        public List<bool> PhantomVehicleInvolved;
+        public List<byte> SpeedLimit;
+        public List<string?> StreetName;
+        public List<bool> ImpairedDriver;
 
         public CrashDateSchema()
         {
-
+            CrashRecordNumber = new List<int>();
+            District = new List<string?>();
+            CrashCounty = new List<string?>();
+            Municipality = new List<string?>();
+            CrashDate = new List<DateTime>();
+            CrashSceneLighting = new List<byte>();
+            Weather = new List<byte>();
+            RoadCondition = new List<byte>();
+            CollisionType = new List<byte>();
+            RelationToRoad = new List<ushort>();
+            IntersectionType = new List<byte>();
+            TrafficControlDeviceType = new List<byte>();
+            UrbanRural = new List<byte>();
+            LocationType = new List<byte>();
+            SchoolBusInvolved = new List<bool>();
+            SchoolZone = new List<bool>();
+            PersonCount = new List<byte>();
+            VehicleCount = new List<byte>();
+            FatalCount = new List<byte>();
+            InjuryCount = new List<byte>();
+            PedestrianCount = new List<byte>();
+            PedestrianDeathCount = new List<byte>();
+            PoliceReportedLatitude = new List<double>();
+            PoliceReportedLongitude = new List<double>();
+            SecondaryResponderLatitude = new List<double>();
+            SecondaryResponderLongitude = new List<double>();
+            PhantomVehicleInvolved = new List<bool>();
+            SpeedLimit = new List<byte>();
+            StreetName = new List<string?>();
+            ImpairedDriver = new List<bool>();
         }
-
-    
     }
 
 }
